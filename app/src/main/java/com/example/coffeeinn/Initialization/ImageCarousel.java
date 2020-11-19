d104b84390bcbd4d0f0a71b924d5f7bd78ce9028
Ryan_A2 <52479838+omnivorse@users.noreply.github.com>
2020-11-20 02:47:05 +0700

    Added button effects. Optimized imports. Changed some texts.

--- app/src/main/java/com/example/coffeeinn/Initialization/ImageCarousel.java
+++ app/src/main/java/com/example/coffeeinn/Initialization/ImageCarousel.java
@@ -1,7 +1,5 @@
 package com.example.coffeeinn.Initialization;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -9,20 +7,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
 import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 import com.example.coffeeinn.MainActivity;
 import com.example.coffeeinn.R;
+import com.squareup.picasso.Picasso;
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
+
+import java.util.ArrayList;
 
 
 public class ImageCarousel extends AppCompatActivity {
 
     CarouselView carouselView;
-    int[] sampleImages = {R.drawable.slide_a, R.drawable.slide_b, R.drawable.slide_c, R.drawable.slide_d};
-    int test = sampleImages.length;
+    int[] sampleImages = {R.drawable.slide_1, R.drawable.slide_2, R.drawable.slide_3, R.drawable.slide_4};
 
-    String prevStarted = "prevStarted";
+
+            String prevStarted = "prevStarted";
     @Override
     protected void onResume() {
         super.onResume();
@@ -40,17 +44,60 @@ public class ImageCarousel extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_carousel);
-        
+
+        final ArrayList<View> viewsToFadeIn = new ArrayList<View>();
+
+        viewsToFadeIn.add(findViewById(R.id.button_start));
+
+        for (View v : viewsToFadeIn)
+        {
+            v.setAlpha(0); // make invisible to start
+        }
+
+
         carouselView = findViewById(R.id.carouselView);
         carouselView.setPageCount(sampleImages.length);
         carouselView.setImageListener(imageListener);
 
+
+        carouselView.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position == 3)
+                {
+                    for (View v : viewsToFadeIn)
+                    {
+                        v.animate().alpha(1.0f).setDuration(250).start();
+                    }
+                }
+
+                else if (position != 3)
+                {
+                    for (View v : viewsToFadeIn)
+                    {
+                        v.animate().alpha(0.0f).setDuration(250).start();
+                    }
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
     }
+
 
     ImageListener imageListener = new ImageListener() {
         @Override
         public void setImageForPosition(int position, ImageView imageView) {
-            imageView.setImageResource(sampleImages[position]);
+            Picasso.get().load(sampleImages[position]).fit().centerCrop().into(imageView);
         }
     };
 
